378db86e7ef917463b2b1534d6fa8491d12a4b46
bidinpithecus <vinibidin@gmail.com>
2022-09-16 18:14:44 -0300

    Feita questão 32

--- trabalho1/zeros/ex32.c
+++ trabalho1/zeros/ex32.c
@@ -1,7 +1,7 @@
 #include "methods.h"
 
-#define V 9.68
-#define R 2.48
+#define V 125.62
+#define R 4.69
 
 double f(double h) {
 	return pow(h,2) * ((-(h * PI))/3 + PI * R) - V;
@@ -19,25 +19,25 @@ int main(void) {
 
 	// Método da Bisseção
 	for(int i = 0; i < sizeof(iterationsBissection) / sizeof(iterationsBissection[0]); i++) {
-		bisection(f, 0, 4.96, iterationsBissection[i]);
+		bisection(f, 0, 9.38, iterationsBissection[i]);
 	}
 	printf("\n");
 
 	// Método de Newton
 	for(int i = 0; i < sizeof(iterationsNewton) / sizeof(iterationsNewton[0]); i++) {
-		newton(f, df, 2.89, iterationsNewton[i]);
+		newton(f, df, 5.02, iterationsNewton[i]);
 	}
 	printf("\n");
 
 	// Método da Secante
 	for(int i = 0; i < sizeof(iterationsSecant) / sizeof(iterationsSecant[0]); i++) {
-		secant(f, 0.89, 4.22, iterationsSecant[i]);
+		secant(f, 0.93, 8.98, iterationsSecant[i]);
 	}
 	printf("\n");
 
 	// Método da Posição Falsa
 	for(int i = 0; i < sizeof(iterationsFalsePosition) / sizeof(iterationsFalsePosition[0]); i++) {
-		falsePosition(f, 0, 4.96, iterationsFalsePosition[i]);
+		falsePosition(f, 0, 9.38, iterationsFalsePosition[i]);
 	}
 	printf("\n");
 
